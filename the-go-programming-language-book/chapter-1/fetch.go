c3b65c12701997989acde4bbcf95d54627d35b94
justin cechmanek <gj.cechmanek@gmail.com>
2022-12-17 10:41:44 -0800

    adds example of grabbing web pages, similar to curl command

---
+++ the-go-programming-language-book/chapter-1/fetch.go
@@ -0,0 +1,28 @@
+// Fetch prints the content found at each specified URL.
+// Call this as 'go run fetch.go http://www.example.com'
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+	}
+}
