aafa7e8deee9b9dc4f094999f89171557e41d4ee
justin cechmanek <gj.cechmanek@gmail.com>
2022-12-29 19:34:42 -0800

    adds basic web server example

---
+++ the-go-programming-language-book/chapter-1/server1.go
@@ -0,0 +1,20 @@
+// server1 is a minimal "echo" server
+// use this with: go run server1.go
+// and in a second terminal: go run fetch.go http://localhost:8000/some_path
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echos the Path component of the requested URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
